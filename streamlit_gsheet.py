9e39be3c4b39e412fc45added8d1eef1911999c0
thien1104 <thien0963303006@gmail.com>
2025-04-02 14:02:57 +0700

    Update streamlit_gsheet.py

--- streamlit_gsheet.py
+++ streamlit_gsheet.py
@@ -91,10 +91,10 @@ with col2:
         }
         @media screen and (max-width: 480px) {
             h2 {
-                font-size: 25px !important;
+                font-size: 22px !important;
             }
             h3 {
-                font-size: 20px !important;
+                font-size: 18px !important;
             }
         }
     </style>
